b67bbfdfac8c280d2ada7dd74902d086023db047
liluredhat <liluredhat@126.com>
2017-05-29 22:38:43 +0800

    test

---
+++ src/main/java/com/hnisi/demo/UserServcie.java
@@ -0,0 +1,36 @@
+package com.hnisi.demo;
+
+import java.util.BitSet;
+
+import redis.clients.jedis.Jedis;
+
+public class UserServcie {
+
+	public boolean login(String username, String password) {
+		//
+		if (username == "lilu" && password == "lilu") {
+			//
+			Jedis jedis = RedisUtil.getJedis();
+			long userid = 0l;
+			jedis.setbit("login:" + "2017-05-29", userid, true);
+			//
+			RedisUtil.returnJedis(jedis);
+			return true;
+		}
+		return false;
+	}
+
+	public int count() {
+		Jedis jedis = RedisUtil.getJedis();
+		BitSet b = BitSet.valueOf(jedis.get("login:2017-05-29".getBytes()));
+		int num = b.cardinality();
+		System.out.println("2017-05-29 login user number: " + num);
+		return num;
+	}
+
+	public static void main(String[] args) {
+		new UserServcie().login("xx", "xx");
+		new UserServcie().login("lilu", "lilu");
+		new UserServcie().count();
+	}
+}
